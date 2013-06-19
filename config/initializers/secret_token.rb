a67495fb9478af398d9c11174c5cfcc9da8b8334
Bogus User <f2529787@rmqkr.net>
2013-06-18 22:21:03 -0300

    Primeiro commit voador

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voador::Application.config.secret_token = 'ea766cf6560d89b794fce479dbaee403097be70f070ad906191e5aff26286d3ace4a6c8ebe4633e93747cc7200df25229d57a8a27d3d45e2aaa829b1d3249f5f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Voador::Application.config.session_store :cookie_store, key: '_voador_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Voador::Application.config.session_store :active_record_store
